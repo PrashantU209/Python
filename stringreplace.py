59113edbc648b0a4e748416e1bfeae01561e60c2
Prashant Kumar Upadhyay <upadhyay.prash20@gmail.com>
2021-08-11 00:27:23 -0400

    Hackerrank mutations

--- stringreplace.py
+++ stringreplace.py
@@ -1,1 +1,19 @@
 #Hackerrank Mutations
+n=input()
+p=input().split()
+A=[]
+for i in n:
+    A.append(i)
+# print(A)
+# print(p)
+# print(int(p[1]))
+c=int(p[1])
+# print(c)
+# print(type(c))
+for j in range(0,len(A)):
+    if j==c:
+        A[j]=p[0]
+# print(A)
+t=""
+y=t.join(A)
+print(y)
